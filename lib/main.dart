292013df3f70d5ac3472a9534ea0b20a7258e545
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-16 18:41:42 +0200

    update main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'core/theme/app_theme.dart';
 import 'core/constants/app_strings.dart';
 import 'features/calculator/ui/calculator_screen.dart';
@@ -22,7 +23,14 @@ class FeedApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: AppStrings.appTitle,
           theme: AppTheme.lightTheme,
-          locale: const Locale('ar'),
+          locale: const Locale('ar', 'EG'),
+          supportedLocales: const [Locale('ar', 'EG'), Locale('en', 'US')],
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+
           home: const CalculatorScreen(),
         );
       },
