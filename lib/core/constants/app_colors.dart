b0bd5dffc72209195e6d64a98dde79931a636f0a
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-16 17:33:20 +0200

    added app colors file

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF2D5A27);
+  static const Color accent = Color(0xFF8BA05F);
+  static const Color background = Color(0xFFF8F9FA);
+  static const Color cardBg = Colors.white;
+  static const Color error = Color(0xFFD32F2F);
+  static const Color textMain = Color(0xFF212121);
+  static const Color textSecondary = Color(0xFF757575);
+  static const Color border = Color(0xFFE0E0E0);
+}
