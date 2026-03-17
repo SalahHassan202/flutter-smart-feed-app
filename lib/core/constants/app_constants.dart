764c7434822004f086b425ab153a7349154687d5
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-17 23:35:28 +0200

    added app constant file

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,18 @@
+import 'app_strings.dart';
+
+class AppConstants {
+  static const Map<String, double> proteinMap = {
+    AppStrings.corn: 10.0,
+    AppStrings.sunflower: 36.0,
+    AppStrings.soy: 46.0,
+    AppStrings.soyHulls: 12.0,
+    AppStrings.bran: 12.0,
+    AppStrings.ragiha: 12.0,
+    AppStrings.glutifeed: 16.0,
+    AppStrings.barley: 12.0,
+    AppStrings.favaBeans: 24.0,
+    AppStrings.molasses: 10.0,
+    AppStrings.additives: 0.0,
+    AppStrings.plasticMixer: 0.0,
+  };
+}
