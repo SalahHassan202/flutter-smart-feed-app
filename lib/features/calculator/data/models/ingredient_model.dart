09bea20014f029a6163347e7e0844a046bd46233
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-17 23:37:14 +0200

    update ingredient_model file

--- lib/features/calculator/data/models/ingredient_model.dart
+++ lib/features/calculator/data/models/ingredient_model.dart
@@ -1,17 +1,35 @@
 class IngredientModel {
-  final String id;
   final String name;
-  final double weight;
-  final double pricePerKilo;
+  double weight;
+  double price;
   final double proteinPercentage;
 
   IngredientModel({
-    required this.id,
     required this.name,
-    required this.weight,
-    required this.pricePerKilo,
+    this.weight = 0.0,
+    this.price = 0.0,
     required this.proteinPercentage,
   });
 
-  double get totalCost => weight * pricePerKilo;
+  double get totalIngredientCost => weight * price;
+
+  double get proteinAmount => (weight * proteinPercentage) / 100;
+
+  Map<String, dynamic> toJson() {
+    return {
+      'name': name,
+      'weight': weight,
+      'price': price,
+      'proteinPercentage': proteinPercentage,
+    };
+  }
+
+  factory IngredientModel.fromJson(Map<String, dynamic> json) {
+    return IngredientModel(
+      name: json['name'],
+      weight: (json['weight'] as num).toDouble(),
+      price: (json['price'] as num).toDouble(),
+      proteinPercentage: (json['proteinPercentage'] as num).toDouble(),
+    );
+  }
 }
