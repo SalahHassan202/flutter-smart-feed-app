79b106486e46a76f0964d92e56785d71588bc3b7
SalahHassan202 <salahhassan66190@gmail.com>
2026-03-18 02:06:14 +0200

    feat: add pastel colors

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -2,12 +2,21 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primary = Color(0xFF2E7D32);
-  static const Color secondary = Color(0xFF4CAF50);
-  static const Color background = Color(0xFFF5F5F5);
+  static const Color background = Color(0xFFF8F9FA);
   static const Color cardBg = Colors.white;
   static const Color textPrimary = Color(0xFF212121);
   static const Color textSecondary = Color(0xFF757575);
   static const Color white = Colors.white;
   static const Color border = Color(0xFFE0E0E0);
-  static const Color accent = Color(0xFFFFC107);
+
+  static const List<Color> pastelColors = [
+    Color(0xFFE3F2FD),
+    Color(0xFFF1F8E9),
+    Color(0xFFFFF3E0),
+    Color(0xFFFCE4EC),
+    Color(0xFFF3E5F5),
+    Color(0xFFE8F5E9),
+    Color(0xFFFFFDE7),
+    Color(0xFFEFEBE9),
+  ];
 }
